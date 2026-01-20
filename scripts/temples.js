2270a11aeaa061dd0bf2b3b529d0490ccceca605
barklin-01 <barklin2003@gmail.com>
2026-01-20 11:23:03 -0500

    new

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,2 +1,17 @@
+//Fecha de Modificacion
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
+
+//  Butoon de Hamburgesa//
+const menuButton = document.querySelector('#menu-button');
+const nav = document.querySelector('nav');
+
+menuButton.addEventListener('click', () => {
+    nav.classList.toggle('open');
+
+    if (nav.classList.contains('open')) {
+        menuButton.innerHTML = '&#10005;'; // X
+    } else {
+        menuButton.innerHTML = '&#9776;'; // ☰
+    }
+});
