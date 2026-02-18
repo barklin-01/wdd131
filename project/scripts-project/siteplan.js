20bc502a473898aecc6183a5f9eb44aa378702e9
barklin-01 <barklin2003@gmail.com>
2026-02-18 00:31:48 -0500

    new

--- project/scripts-project/siteplan.js
+++ project/scripts-project/siteplan.js
@@ -2,13 +2,13 @@
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
 //boton de hamburgesa//
-const menuButton = document.getElementById('menu-button');
-const nav = document.querySelector('header nav');
+const menuButton = document.getElementById("menu-button");
+const nav = document.querySelector("nav");
 
-menuButton.addEventListener('click', () => {
-    if (nav.style.display === 'flex') {
-        nav.style.display = 'none';
+menuButton.addEventListener("click", () => {
+    if (nav.style.display === "block") {
+        nav.style.display = "none";
     } else {
-        nav.style.display = 'flex';
+        nav.style.display = "block";
     }
 });
